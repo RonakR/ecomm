0e5a2e0ecc62b442eaf15c89f10500f8d9249432
Ronak Raithatha <ronaksraithatha@gmail.com>
2023-04-17 15:34:36 -0400

    added tracing and middleware

--- app.js
+++ app.js
@@ -2,18 +2,53 @@
 import * as dotenv from 'dotenv'
 dotenv.config()
 
-// express
-import Express from "express";
-const app = Express()
+// postman
+import postman from '@postman/postman-sdk'
+// treblle
+import treblle from '@treblle/express'
 
 // db
 import connectDB from "./db/connect.js"
 
+// express
+import express from "express";
+import * as expressAsyncErrors from 'express-async-errors'
+const app = express()
+
+// middleware
+import notFoundMiddleware from './middleware/not-found.js'
+import errorHandlerMiddleware from './middleware/error-handler.js'
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('e-comm api')
+})
+
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
+
 const port = process.env.PORT || 5000
 const start = async () => {
   try {
+    // db connect
     await connectDB(process.env.MONGO_URL)
+    // server start
     app.listen(port, console.log(`Server is listening on port ${port}...`))
+    // postman telemetry
+    postman.initialize({
+      collectionId: '24422144-78465df7-cb85-4077-b864-80a0638f6af8',
+      apiKey: process.env.POSTMAN_API_KEY,
+      enable: true
+    });
+    // treblle telemetry
+    app.use(
+      treblle({
+        apiKey: process.env.TREBLLE_API_KEY,
+        projectId: process.env.TREBLLE_PROJECT_ID,
+        additionalFieldsToMask: [],
+      })
+    )
   } catch (error) {
     console.log(error)
   }
--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,5 +1,6 @@
-const { StatusCodes } = require('http-status-codes');
-const errorHandlerMiddleware = (err, req, res, next) => {
+import { StatusCodes } from 'http-status-codes'
+
+export default function errorHandlerMiddleware (err, req, res, next) {
   let customError = {
     // set default
     statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
@@ -24,5 +25,3 @@ const errorHandlerMiddleware = (err, req, res, next) => {
 
   return res.status(customError.statusCode).json({ msg: customError.msg });
 };
-
-module.exports = errorHandlerMiddleware;
