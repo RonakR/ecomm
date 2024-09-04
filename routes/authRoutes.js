5e83944dd69b2eddcaaf163915267a696880b496
Ronak Raithatha <ronaksraithatha@gmail.com>
2024-09-04 12:48:37 -0400

    added register again

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 const { register, login, logout } = require('../controllers/authController')
 
-// router.post('/auth/register', register)
+router.post('/auth/register', register)
 router.post('/auth/login', login)
 router.get('/auth/logout', logout)
 
